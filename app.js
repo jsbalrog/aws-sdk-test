d55dc015e257106269cd40f3c6bc5ea9892ff2b3
jsbalrog <edwardtjenkins@gmail.com>
2014-11-30 10:39:57 -0700

    Another commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ s3.listBuckets(function(err, data) {
   }
 });
 
+// Now get an image
 var params = { Bucket: 'teds-test-bucket', Key: 'gandalf lotr macbook apple.jpg' };
 var file = require('fs').createWriteStream('/Users/gandalf/dev/amazon-sdk-test/image.jpg');
 s3.getObject(params).createReadStream().pipe(file);
