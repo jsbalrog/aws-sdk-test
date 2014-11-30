e5e87cdb21a35f9afd12fd17b8d0371ca8ca726b
Ted Jenkins <gandalf@middle-earth.hsd1.ut.comcast.net>
2014-11-30 10:33:24 -0700

    Added another example

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 
-AWS.config.region = 'us-west-1';
+// AWS.config.region = 'us-west-1';
 var s3 = new AWS.S3();
 
 s3.listBuckets(function(err, data) {
@@ -9,3 +9,7 @@ s3.listBuckets(function(err, data) {
     console.log("Bucket: " , bucket.Name, ': ', bucket.CreationDate);
   }
 });
+
+var params = { Bucket: 'teds-test-bucket', Key: 'gandalf lotr macbook apple.jpg' };
+var file = require('fs').createWriteStream('/Users/gandalf/dev/amazon-sdk-test/image.jpg');
+s3.getObject(params).createReadStream().pipe(file);
